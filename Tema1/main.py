d696432d2f6151d48d47799dcd41103c08662144
robertadriang <58529493+robertadriang@users.noreply.github.com>
2022-02-23 09:20:31 +0200

    Initial commit

---
+++ Tema1/main.py
@@ -0,0 +1,158 @@
+import math
+import random
+from decimal import Decimal
+
+
+# Exercise 1
+def machine_precision():
+    u = 1
+    count = 0
+    while 1 + u != 1:
+        u /= 10
+        count += 1
+        # print(f"U:{u}, Count:{count}, 1+u={1+u}")
+    return u * 10, count - 1
+
+
+# Exercise 2
+def custom_addition(u):
+    a = 1.0
+    b = u / 10
+    c = u / 10
+    print("(a+b)+c = ", (a + b) + c)
+    print("(a+b)+c = ", (a + (b + c)))
+
+
+def custom_product():
+    while True:
+        a = random.random()
+        b = random.random()
+        c = random.random()
+
+        product_1 = (a * b) * c
+        product_2 = a * (b * c)
+        if product_1 != product_2:
+            print("a=", a)
+            print("b=", b)
+            print("c=", c)
+            print('(a*b)*c = {:.20}'.format(product_1))
+            print('a*(b*c) = {:.20}'.format(product_2))
+            break
+
+
+# Exercise 3
+### Parse the given text file and return a dictionary of params
+def parse_file(file):
+    with open('date_T1.txt', 'r') as fp:
+        lines = fp.readlines()
+        available_functions = ['Sinus', 'Cosinus', 'Ln']
+        available_terms = ['a', 'b']
+
+        current_function = None
+        current_term = None
+
+        functions_params = dict()
+
+        for line in lines:
+            if line.strip() in available_functions:
+                current_function = line.strip()
+                functions_params[current_function] = dict()
+                continue
+            elif line.strip() in available_terms:
+                current_term = line.strip()
+                functions_params[current_function][current_term] = []
+                continue
+            else:
+                functions_params[current_function][current_term].append(Decimal(line.strip()))
+        return functions_params
+
+
+### Compute Q or P polynom based on
+# y - the input param
+# function - Sinus/Cosinus/Ln
+# polynom_type (P or Q)
+def compute_polynom(y, function, polynom_type):
+    global p
+    if polynom_type == 'P_4':
+        return p[function]['a'][0] + y * (p[function]['a'][1] + y * (
+                p[function]['a'][2] + y * (p[function]['a'][3] + y * p[function]['a'][4])))
+    elif polynom_type == 'Q_4':
+        return p[function]['b'][0] + y * (p[function]['b'][1] + y * (
+                p[function]['b'][2] + y * (p[function]['b'][3] + y * p[function]['b'][4])))
+
+
+### When a numbers is between -(10^-12) and (10^-12) replace it with -(10^-12) or (10^-12)
+def replace_small_numbers(a):
+    if abs(a) < 10 ** (-12):
+        if a < 0:
+            return -1 * (10 ** (-12))
+        return 10 ** (-12)
+    return a
+
+
+def sinus_approximation(x):
+    if x < -1 or x > 1:
+        print("Error! -1 <= x<= 1 for sinus")
+        return
+    return x * compute_polynom(x * x, 'Sinus', 'P_4') / replace_small_numbers(compute_polynom(x * x, 'Sinus', 'Q_4'))
+
+
+def cosinus_approximation(x):
+    if x < -1 or x > 1:
+        print("Error! -1 <= x<= 1 for cosinus")
+        return
+    return compute_polynom(x * x, 'Cosinus', 'P_4') / replace_small_numbers(compute_polynom(x * x, 'Cosinus', 'Q_4'))
+
+
+def ln_approximation(x):
+    if x < 1 / (2 ** (1 / 2)) or x > 2 ** (1 / 2):
+        print("Error! 1/sqrt(2) <= x<= sqrt(2) for ln")
+        return
+    z = Decimal((x - 1) / replace_small_numbers(x + 1))
+    return z * compute_polynom(z * z, 'Ln', 'P_4') / replace_small_numbers(compute_polynom(z * z, 'Ln', 'Q_4'))
+
+
+def sin_stats(x):
+    library_result = math.sin(Decimal(math.pi * 1 / 4 * x))
+    approximated_result = sinus_approximation(Decimal(x))
+    print(f"[SIN] Inpus argument: {x}")
+    print("[SIN]Result from library=", library_result)
+    print("[SIN] Approximated result=", approximated_result)
+    print("[SIN] Difference between results=", abs(Decimal(library_result) - approximated_result))
+    print('')
+
+
+def cos_stats(x):
+    library_result = math.cos(Decimal(math.pi * 1 / 4 * x))
+    approximated_result = cosinus_approximation(Decimal(x))
+    print(f"[COS] Input argument: {x}")
+    print("[COS]Result from library=", library_result)
+    print("[COS] Approximated result=", approximated_result)
+    print("[COS] Difference between results=", abs(Decimal(library_result) - approximated_result))
+    print('')
+
+
+def ln_stats(x):
+    library_result = math.log(1.1)
+    approximated_result = ln_approximation(1.1)
+    print(f"[LN] Input argument: {x}")
+    print("[LN]Result from library=", library_result)
+    print("[LN] Approximated result=", approximated_result)
+    print("[LN] Difference between results=", abs(Decimal(library_result) - approximated_result))
+    print('')
+
+
+if __name__ == '__main__':
+    global p
+    u, count = machine_precision()
+    print(f"u:{u}, count={count}")
+    custom_addition(u)
+    custom_product()
+    p = parse_file('date_T1.txt')
+    print(p)
+
+    x = 0.66
+    sin_stats(x)
+    cos_stats(x)
+    x = 1.23
+    ln_stats(x)
---
+++ Tema2/main.py
@@ -0,0 +1,70 @@
+import numpy as np
+
+
+def define_parameters(n, eps, A, b):
+    print(n)
+    print(eps)
+    print(A)
+    print(b)
+
+
+def search_pivot(matrix, l):
+    return np.argmax(abs(matrix[l:, l]))+l
+
+
+
+def swap_lines(matrix, l1, l2):
+    matrix[[l1, l2]] = matrix[[l2, l1]]
+    # print(matrix)
+    return matrix
+
+
+def make_superior_triangular(matrix):
+    for i in range(len(matrix)):
+        for j in range(i):
+            matrix[i,j]=0
+    return matrix
+
+def g_e_p_p(n, eps, A, b):
+    print("Matrix size:", n)
+    print("Precision:", eps)
+    print("A:", A)
+    print("b:", b)
+
+    A_b = np.append(A, b, axis=1)
+    A_b=A_b.astype(np.float64)
+
+    l = 0
+    line_to_swap = search_pivot(A_b, l)
+    if line_to_swap != l:
+        A_b = swap_lines(A_b, l, line_to_swap)
+
+    while l < n - 1 and abs(A_b[l, l]) > eps:
+        print("A_B at while start",A_b)
+        for i in range(l + 1, n):
+
+            A_b[i, l] = A_b[i, l] / A_b[l, l]
+            print("After:",A_b)
+            for j in range(l + 1, n + 1):
+                A_b[i, j] = A_b[i, j] - A_b[i, l] * A_b[l, j]
+        l += 1
+        line_to_swap = search_pivot(A_b, l)
+        print("A_b before:", A_b)
+        print("Line_to_swap:", line_to_swap)
+        print("A_b after:", A_b)
+        if line_to_swap != l:
+            A_b = swap_lines(A_b, l, line_to_swap)
+
+    if abs(A_b[l, l]) <= eps:
+        print("Matricea este singulara")
+    else:
+        A_b=make_superior_triangular(A_b)
+        print("tre sa mai citim :(")
+
+
+if __name__ == '__main__':
+    print("Tema 2")
+    # define_parameters(3,10**-6,np.matrix([[1,-1,1],[-6,1,-1],[3,1,1]]),np.array([2,3,4]))
+    g_e_p_p(3, 10 ** -6, np.array([[2, 0, 1], [0, 2, 1], [4, 4, 6]]),np.array([[5], [1], [14]]))
+    #g_e_p_p(3, 10 ** -6, np.array([[2.0, 0.0, 1.0], [0.0, 2.0, 1.0], [4.0, 4.0, 6.0]]), np.array([[5.0], [1.0], [14.0]]))
+    #g_e_p_p(4, 10 ** -6, np.matrix([[0.02, 0.01, 0,0], [1, 2, 1,0], [0, 1, 2,1], [0, 0, 100, 200]]), np.array([[0.02], [1], [4],[800]]))

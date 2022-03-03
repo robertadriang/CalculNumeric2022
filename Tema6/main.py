7d28b9ad7c361d1f7120aeb468c8977bb8cd785c
robertadriang <58529493+robertadriang@users.noreply.github.com>
2022-03-03 11:42:30 +0200

    Rezolvat problema la atkin (+ precizie)

--- Tema6/main.py
+++ Tema6/main.py
@@ -15,16 +15,16 @@ def f3(x):
     return x ** 3 - 3 * x + 15
 
 
-def generate_points_from_interval(start, end, n):
+def generate_points_from_interval(start, end, n,decimals):
     result_set = set()
     result_set.add(start)
     result_set.add(end)
     while len(result_set) <= n:
-        result_set.add(random.uniform(start, end))
+        result_set.add(round(random.uniform(start, end),ndigits=decimals))
     return list(sorted(list(result_set)))
 
 
-def generate_aitken_schema(x, y):
+def generate_aitken_schema(x, y,eps):
     print(x)
     print(y)
     aitken_schema = np.zeros((len(x), len(y) - 1), dtype=float)
@@ -38,18 +38,23 @@ def generate_aitken_schema(x, y):
     for j in range(1, len(aitken_schema[0])):
         for i in range(j + 1, len(aitken_schema)):
             #### AICI E PROBABIL O EROARE :(
-            aitken_schema[i][j] = (aitken_schema[i][j - 1] - aitken_schema[i - 1][j - 1]) / (x[i] - x[0])
+            aitken_schema[i][j] = check_precision((aitken_schema[i][j - 1] - aitken_schema[i - 1][j - 1]) / (x[i] - x[i-j-1]),eps)
     print(aitken_schema)
     return np.insert(np.diagonal(aitken_schema, -1), 0, y[0])
 
 
-def compute_L_polynom(value, x_array, y_array):
+def compute_L_polynom(value, x_array, y_array,eps=10**-8):
     Ln = y_array[0]
     right_term = 1
     for i in range(1, len(y_array)):
         right_term *= (value - x_array[i - 1])
-        Ln += y_array[i] * right_term
+        Ln += check_precision(y_array[i] * right_term,eps)
     return Ln
+
+def check_precision(a, eps):
+    if abs(a) < abs(eps):
+        return 0
+    return a
 
 
 # utilizand forma Newton a polinomului de interpolare Lagrange ¸si schema
@@ -57,19 +62,22 @@ def compute_L_polynom(value, x_array, y_array):
 # |Ln(¯x) − f(¯x)|;
 if __name__ == '__main__':
     print("Tema 6")
+
+    precision=6
     x_1_0 = 1
     x_1_n = 5
 
     print(f1(x_1_0))
-    points = generate_points_from_interval(x_1_0, x_1_n, 10)
-    points = [1, 2, 3, 4, 5]
+    points = generate_points_from_interval(x_1_0, x_1_n, 100,decimals=precision)
+    #points = [1, 2, 3, 4, 5]
     results = [f1(e) for e in points]
-    new_y = generate_aitken_schema(points, results)
+    new_y = generate_aitken_schema(points, results,eps=10**-precision)
     print(new_y)
 
-    for i in points:
+    check_points=[i*0.2 for i in range(20)]
+    for i in check_points:
         print("I:", i)
-        approximated= compute_L_polynom(i, points, new_y)
+        approximated= compute_L_polynom(i, points, new_y,eps=10**-precision)
         print("Aproximated:",approximated)
         print("Function result:", f1(i))
         print("Error:", abs(approximated - f1(i)))
